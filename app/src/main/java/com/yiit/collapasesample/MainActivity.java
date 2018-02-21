f17336177128adb505e03587d8306fa511656fef
yiitandroid <mtejaswi@yiit.co>
2018-02-21 16:50:25 +0530

    hai plz do this

---
+++ app/src/main/java/com/yiit/collapasesample/MainActivity.java
@@ -0,0 +1,22 @@
+package com.yiit.collapasesample;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Admin on 21-02-2018.
+ */
+
+public class MainActivity extends AppCompatActivity{
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
